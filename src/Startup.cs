99f8c903d4f1820ef834405d53e05dad302ccd47
makeevnikita <makeevnikita2001@inbox.ru>
2023-06-25 20:43:47 +0700

    Создал файлы Startup.cs и Program.cs

---
+++ src/Startup.cs
@@ -0,0 +1,29 @@
+namespace src
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
